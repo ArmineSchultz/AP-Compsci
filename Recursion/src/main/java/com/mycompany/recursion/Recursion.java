b008c714f6e3accc868fe4baebffdec6d33ef932
Armine <aschultz2026@cchsdons.com>
2026-03-12 09:18:16 -0700

    Add files via upload

---
+++ Recursion/src/main/java/com/mycompany/recursion/Recursion.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.recursion;
+
+/**
+ *
+ * @author ASchultz2026
+ */
+public class Recursion {
+
+    public static void main(String[] args) {
+        PascalsTriangle triangle = new PascalsTriangle(5);
+        triangle.solve();
+    }
+}
+
+class PascalsTriangle {
+    private int nthLine = 0;
+    
+    public PascalsTriangle( int totalLines){
+        totalLines = nthLine;
+    }
+    public void solve(){
+        
+    }        
+                
+
+}
